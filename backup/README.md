43a4caefd8ee4acb484e8467252304f7ff14cfc1
Thomas Toye <thomas@toye.io>
2018-02-26 17:44:18 +0100

    Add host backup option

---
+++ backup/README.md
@@ -0,0 +1,8 @@
+# Example `.env`
+
+```
+CLOUDANT_USERNAME=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx-bluemix
+CLOUDANT_PASSWORD=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+CLOUDANT_HOST=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx-bluemix.cloudant.com
+```
+
--- backup/backup.js
+++ backup/backup.js
@@ -8,10 +8,11 @@ const fs = require('fs');
 
 const username = process.env.CLOUDANT_USERNAME;
 const password = process.env.CLOUDANT_PASSWORD;
+const host     = process.env.CLOUDANT_HOST;
 
 const cloudant = Cloudant({ account: username, password: password});
 
-const url = `https://${username}:${password}@${username}.cloudant.com/`;
+const url = `https://${username}:${password}@${host}.cloudant.com/`;
 
 if(!fs.existsSync('backups')) {
   fs.mkdirSync('backups')
